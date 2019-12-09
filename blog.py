f5d3273a5f50ace7844a1a95b05cc4d148c3e0a6
Chinmay4400 <https://github.com/Chinmay4400>
2019-12-10 01:06:48 +0530

    Added HTML templates Added CSS

--- blog.py
+++ blog.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Chinmay Badjatya',
+        'title' : 'FirstPost' ,
+        'content' : 'First Post stuff',
+        'DatePosted' : '9th December',
+    },
+    {
+        'author' : 'Elon Musk',
+        'title' : 'SecondPost' ,
+        'content' : 'SecondPost stuff',
+        'DatePosted' : '9th December',
+    }
+]
+
 @app.route("/")
-def hello():
-    return "Hello World"
+@app.route("/home")
+def home():
+    return render_template('home.html', posts = posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == 'main':
+    app.run(debug=True)
