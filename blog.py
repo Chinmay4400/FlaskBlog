1eb2836a7968d73ee63ba5553436feeb25bb1b32
Chinmay4400 <https://github.com/Chinmay4400>
2019-12-13 19:39:33 +0530

    changed folder

--- blog.py
+++ blog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import registrationForm, loginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '26ed3156048a4eb65dd07ce47fee898d'
 
 posts = [
     {
@@ -25,6 +28,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = registrationForm()
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login")
+def login():
+    form = loginForm()
+    return render_template('login.html', title = 'Login', form=form)
 
 if __name__ == 'main':
     app.run(debug=True)
