53afebadff64226d4c218b2e37d11fe1bc38256f
Chinmay4400 <https://github.com/Chinmay4400>
2019-12-18 21:45:37 +0530

    Added functionality for adding, updating and deleting posts

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User
 from flask_login import current_user
@@ -63,3 +63,9 @@ class UpdateAccountForm(Form):
             email = User.query.filter_by(email=email.data).first()
             if email:
                 raise ValidationError('This email is already associated with an account. Please try again.')
+        
+    
+class PostForm(Form):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
