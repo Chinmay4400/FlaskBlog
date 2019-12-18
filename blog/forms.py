f7c0e8107949ffdcce38f7dfdb2035c7a754b302
Chinmay4400 <https://github.com/Chinmay4400>
2019-12-18 11:57:37 +0530

    added functionality to update user profile

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import Form
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from blog.models import User
+from flask_login import current_user
 
 class registrationForm(Form):
     username = StringField('Username', 
@@ -40,3 +42,24 @@ class loginForm(Form):
         email = User.query.filter_by(email=email.data).first()
         if not email:
             raise ValidationError('This email is not in the database. Please sign up first.')
+    
+
+class UpdateAccountForm(Form):
+    username = StringField('Username', 
+                            validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                            validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data !=  current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is not available. Please try again.')
+            
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('This email is already associated with an account. Please try again.')
